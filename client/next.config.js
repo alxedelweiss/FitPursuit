cdb4ede6fc01bd92dfb68766161da5ba2d29a4bc
Alexander Galev <alex.galev@yahoo.com>
2025-01-31 17:35:35 -0800

    obscure backend URL

--- client/next.config.js
+++ client/next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	experimental: {
-		appDir: true
+	  appDir: true
 	},
 	async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: 'https://v0ww4ks.ironforgesoftware.com/api/:path*' // Proxy to Backend
-			}
-		]
+	  return [
+		{
+		  source: '/api/:path*',
+		  destination: `${process.env.BACKEND_URL}/api/:path*` // Proxy to Backend
+		}
+	  ];
 	}
-}
-
-module.exports = nextConfig
+  };
+  
+  module.exports = nextConfig;
+  
